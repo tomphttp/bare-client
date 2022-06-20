fce00152597d8f5099de7cff2a494469349b5656
David Reed <x@sys32.dev>
2022-06-20 14:34:58 -0400

    prepare script, cleanup rollup config, adopt md5, 1.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -1,64 +1,35 @@
 import { resolve } from 'path';
 
 import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
 import inject from '@rollup/plugin-inject';
-import nodeResolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
-const snapshot = Object.fromEntries(
-	['fetch', 'Request', 'Response', 'WebSocket', 'XMLHttpRequest'].map(name => [
-		resolve('src/snapshot.ts'),
-		name,
-	])
-);
-
 export default [
-	// ES Modules
-	{
-		input: 'src/index.ts',
-		output: {
-			file: 'dist/BareClient.es.js',
-			format: 'es',
-			exports: 'default',
-			sourcemap: true,
-		},
-		plugins: [
-			inject({
-				...snapshot,
-			}),
-			nodeResolve({ browser: true }),
-			commonjs(),
-			typescript(),
-			babel({ babelHelpers: 'bundled', extensions: ['.ts'] }),
-		],
-	},
-
-	// UMD
-	{
-		input: 'src/index.ts',
-		output: {
-			file: 'dist/BareClient.umd.min.js',
-			format: 'umd',
-			exports: 'default',
-			name: 'BareClient',
-			indent: false,
-			sourcemap: true,
-		},
-		plugins: [
-			inject({
-				...snapshot,
-			}),
-			nodeResolve({ browser: true }),
-			commonjs(),
-			typescript(),
-			babel({
-				babelHelpers: 'bundled',
-				extensions: ['.ts'],
-				exclude: 'node_modules/**',
-			}),
-			terser(),
-		],
-	},
-];
+	['esm', 'src/BareClient.ts', 'named', [false]], // import
+	['umd', 'src/index.ts', 'default', [(true, false)]], // require, minify for browser
+]
+	.map(([format, input, exports, modes]) =>
+		modes.map(minify => ({
+			input,
+			output: {
+				file: `dist/BareClient.${format}${minify ? '.min' : ''}.js`,
+				format,
+				name: 'BareClient',
+				exports,
+			},
+			plugins: [
+				inject(
+					Object.fromEntries(
+						['fetch', 'Request', 'Response', 'WebSocket', 'XMLHttpRequest'].map(
+							name => [resolve('src/snapshot.ts'), name]
+						)
+					)
+				),
+				typescript(),
+				babel({ babelHelpers: 'bundled', extensions: ['.ts'] }),
+				minify && terser(),
+			],
+		}))
+	)
+	.flat(1);
