5cfc1742cd01e723c902f2ff4f011e5e16bbdf6b
David Reed <reedswork@proton.me>
2023-06-14 12:18:01 -0400

    unused

--- src/webSocket.ts
+++ src/webSocket.ts
@@ -1,6 +1,9 @@
+/*
+ * WebSocket helpers
+ */
+
 const validChars =
 	"!#$%&'*+-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ^_`abcdefghijklmnopqrstuvwxyz|~";
-const reserveChar = '%';
 
 export function validProtocol(protocol: string): boolean {
 	for (let i = 0; i < protocol.length; i++) {
@@ -12,41 +15,4 @@ export function validProtocol(protocol: string): boolean {
 	}
 
 	return true;
-}
-
-export function encodeProtocol(protocol: string): string {
-	let result = '';
-
-	for (let i = 0; i < protocol.length; i++) {
-		const char = protocol[i];
-
-		if (validChars.includes(char) && char !== reserveChar) {
-			result += char;
-		} else {
-			const code = char.charCodeAt(0);
-			result += reserveChar + code.toString(16).padStart(2, '0');
-		}
-	}
-
-	return result;
-}
-
-export function decodeProtocol(protocol: string): string {
-	let result = '';
-
-	for (let i = 0; i < protocol.length; i++) {
-		const char = protocol[i];
-
-		if (char === reserveChar) {
-			const code = parseInt(protocol.slice(i + 1, i + 3), 16);
-			const decoded = String.fromCharCode(code);
-
-			result += decoded;
-			i += 2;
-		} else {
-			result += char;
-		}
-	}
-
-	return result;
 }
